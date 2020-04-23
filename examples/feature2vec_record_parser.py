90ea239fd321738fb25ce54216cf0091fd5d9043
xuezhengyin210834 <xuezhengyin210834@sohu-inc.com>
2020-04-23 10:50:53 +0800

    feat:feat2vec parser
    update feat2vec parser

--- examples/feature2vec_record_parser.py
+++ examples/feature2vec_record_parser.py
@@ -6,41 +6,70 @@ import tensorflow as tf
 from deeprec.dataset.parser import BaseRecordParser, ParserType
 
 
+def _parse_weight_mean(line) -> str:
+    res = line.strip().split(" ")
+    uid = bytes(res[0], encoding="utf-8")
+    input_data = res[1].split(";")
+    vid = int(input_data[0])
+    third_category = int(input_data[1])
+    tag = int(input_data[2].split(","))
+    kis = int(input_data[3])
+    album = int(input_data[4])
+    output_data = res[2].split(";")
+    output = int(output_data[0])
+
+    feature = {
+        "uid": Feature2vecRecordParser.bytes_list_feature([uid]),
+        "vid": Feature2vecRecordParser.int64_list_feature([vid]),
+        "third_category": Feature2vecRecordParser.int64_list_feature(([third_category])),
+        "tag": Feature2vecRecordParser.int64_list_feature([tag]),
+        "kis": Feature2vecRecordParser.int64_list_feature([kis]),
+        "album": Feature2vecRecordParser.int64_list_feature([album]),
+        "output": Feature2vecRecordParser.int64_list_feature([output])
+    }
+
+    serial_string = tf.train.Example(features=tf.train.Features(feature=feature))
+    return serial_string.SerializeToString()
+
+
+def _parse_concat(line) -> str:
+    res = line.strip().split(" ")
+    uid = bytes(res[0], encoding="utf-8")
+
+    item_features = [int(x) for x in res[1].split(";")]
+    feature = {
+        "uid": Feature2vecRecordParser.bytes_list_feature([uid]),
+        "features": Feature2vecRecordParser.int64_list_feature(item_features)
+    }
+
+    serial_string = tf.train.Example(features=tf.train.Features(feature=feature))
+    return serial_string.SerializeToString()
+
+
 class Feature2vecRecordParser(BaseRecordParser):
 
     def __init__(self, path,
                  output,
+                 combine="concat",
                  prefix="feature2vec",
                  suffix="record",
                  size=50000,
                  process=ParserType.TRAIN,
                  verbose=True):
+        self._combine = combine
         super(Feature2vecRecordParser, self).__init__(path, output, prefix, suffix, size, process, verbose)
 
-    def parse_train(self, line):
-        res = line.strip().split(" ")
-        uid = bytes(res[0], encoding="utf-8")
-        input_data = res[1].split(";")
-        vid = int(input_data[0])
-        third_category = int(input_data[1])
-        tag = int(input_data[2].split(","))
-        kis = int(input_data[3])
-        album = int(input_data[4])
-        output_data = res[2].split(";")
-        output = int(output_data[0])
+    def parse_train(self, line) -> str:
+        """
 
-        feature = {
-            "uid": Feature2vecRecordParser.bytes_list_feature([uid]),
-            "vid": Feature2vecRecordParser.int64_list_feature([vid]),
-            "third_category": Feature2vecRecordParser.int64_list_feature(([third_category])),
-            "tag": Feature2vecRecordParser.int64_list_feature([tag]),
-            "kis": Feature2vecRecordParser.int64_list_feature([kis]),
-            "album": Feature2vecRecordParser.int64_list_feature([album]),
-            "output": Feature2vecRecordParser.int64_list_feature([output])
-        }
+        :param line: input data record line
+        :return: serial string of record
+        """
+        if self._combine == "concat":
+            return _parse_concat(line)
 
-        serial_string = tf.train.Example(features=tf.train.Features(feature=feature))
-        return serial_string.SerializeToString()
+        if self._combine == "combine":
+            return _parse_weight_mean(line)
 
     def parse_predict(self, line):
         pass
@@ -48,12 +77,14 @@ class Feature2vecRecordParser(BaseRecordParser):
     def parse_test(self, line):
         pass
 
+
 if __name__ == "__main__":
     ap = argparse.ArgumentParser()
     ap.add_argument("--input", default=None, type=str, help="input path of train dataset", required=True)
     ap.add_argument("--output", default=None, type=str, help="output path of record", required=True)
     ap.add_argument("--process", default=None, type=str, help="process of parser:[TRAIN, PREDICT]", required=True)
     ap.add_argument("--size", default=50000, type=int, help="size of record file")
+    ap.add_argument("--combine", default=None, type=str, help="method of combine feature vector", required=True)
 
     args = vars(ap.parse_args())
     if args["process"] == "TRAIN":
